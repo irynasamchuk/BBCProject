3cb2a92a92b60abcfab693d30864570d33cba7f1
Iryna Sam <irinasamcuk@gmail.com>
2019-11-27 10:14:36 +0200

    Delete lines

--- BBCTestProject/BBCTestProject/PageObject/HomePage.cs
+++ BBCTestProject/BBCTestProject/PageObject/HomePage.cs
@@ -9,9 +9,5 @@ namespace BBCTestProject.PageObject
             Browser.Get().FindElements( By.XPath( "//div[contains(@class, 'orb-nav-section') and contains(@class, 'orb-nav-links')]//li" ) );
 
         public IWebElement Header => Browser.Get().FindElement( By.XPath( "//div[@id = 'bbcle-header-masthead']/h1" ) );
-
-
-
-
     }
 }
